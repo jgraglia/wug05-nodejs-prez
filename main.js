6c55aec3ef6bb822c710214b699ca1e72f382726
Julien Graglia <jgraglia@netceler.com>
2012-05-01 22:26:10 +0200

    winston

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-console.log("Hello World!");
+winston = require("winston");
+
+var logger = new (winston.Logger)({
+		transports: [
+			new winston.transports.File(
+				{ 
+					filename: './logs/all-logs.log', 
+					timestamp: true,
+					maxsize: 1000000 
+				}
+			),
+			new winston.transports.Console(
+				{
+					colorize:  true, 
+					timestamp: true, 
+					handleExceptions: true
+				}
+			)
+		],
+		exceptionHandlers: [
+			new winston.transports.File({ filename: './logs/exceptions.log', timestamp: true, maxsize: 1000000 })
+		],
+	exitOnError: true,
+	}
+);
+
+
+
+logger.log("info", "Bonjour le monde!");
+
+setTimeout( function () {
+		nonExistingFunction();
+	}, 2000);
+
+setInterval( function () {
+		logger.log("info", "Merhaba!");
+	}, 1000);
+
